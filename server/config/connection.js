10c13b0717974d654faac83de6ec44e42a31c9f2
Rogers Ramirez <rogers.ramirez2008@gmail.com>
2021-01-25 21:24:27 -0500

    Deploying app into Heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
 
-
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify: true,
   useCreateIndex: true,
+  useFindAndModify: true,
 });
-
 
 module.exports = mongoose.connection;
