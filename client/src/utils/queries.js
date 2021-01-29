a2e692a2d4644a7bf6f46307efade20d2355b386
Rogers Ramirez <rogers.ramirez2008@gmail.com>
2021-01-29 18:43:21 -0500

    Created the mutations and query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,70 +1,17 @@
 import gql from 'graphql-tag';
 
 export const QUERY_PRODUCTS = gql`
-  query getProducts($category: ID) {
-    products(category: $category) {
+  query {
+    me {
       _id
-      name
-      description
-      price
-      quantity
-      image
-      category {
+      email
+      firstName
+      lastName
+      appointment{
         _id
+        day
+        time
       }
-    }
-  }
-`;
-
-export const QUERY_ALL_PRODUCTS = gql`
-  {
-    products {
-      _id
-      name
-      description
-      price
-      quantity
-      category {
-        name
-      }
-    }
-  }
-`;
-
-export const QUERY_CATEGORIES = gql`
-{
-  categories {
-    _id
-    name
-  }
-}
-`;
-
-export const QUERY_USER = gql`
-{
-  user {
-    firstName
-    lastName
-    orders {
-      _id
-      purchaseDate
-      products {
-        _id
-        name
-        description
-        price
-        quantity
-        image
-      }
-    }
-  }
-}
-`;
-
-export const QUERY_CHECKOUT = gql`
-  query getCheckout($products: [ID]!) {
-    checkout(products: $products) {
-      session
     }
   }
 `;
