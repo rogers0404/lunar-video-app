9c2d5858cbcf698b124b6a7b126ecff9fbaf97fc
Rogers Ramirez <rogers.ramirez2008@gmail.com>
2021-01-30 08:38:30 -0500

    Added login functionality

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -13,8 +13,18 @@ function Nav() {
                 :
                 <a href="/login">Log In</a>
             }
-          <a href="/signup">Sign up</a>
-          <a href="/schedule">Schedulle</a>
+            {
+              Auth.loggedIn() ?
+              <a href="/signup">Sign up</a>
+                :
+                <a href="/signup" onclick={"return false;"}>Sign up</a>
+            }
+            {
+              Auth.loggedIn() ?
+              <a href="/schedule">Schedulle</a>
+                :
+                <a href="/" onclick={"return false;"}>Schedulle</a>
+            }
           <a href="#contact">Contact</a>
           <a href="#about">About</a>
         </div>
