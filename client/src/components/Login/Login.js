97497cb8c75b2e53efd51c82a761e58ab456035b
Rogers Ramirez <rogers.ramirez2008@gmail.com>
2021-01-30 09:27:56 -0500

    Added login functionality

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -3,6 +3,7 @@ import { useMutation } from '@apollo/react-hooks';
 import { Link } from "react-router-dom";
 import { LOGIN } from "../../utils/mutations"
 import Auth from "../../utils/auth";
+import { Container, Text, Heading, FormControl, FormLabel, Input, Button } from "@chakra-ui/react";
 
 function Login(props) {
   const [formState, setFormState] = useState({ email: '', password: '' })
@@ -33,8 +34,36 @@ function Login(props) {
         ← Go to Signup
       </Link>
 
-      <h2>Login</h2>
-      <form onSubmit={handleFormSubmit}>
+      <Heading as="h2" size="xl" fontSize={{ base: "20px", md: "34px", lg: "40px" }} padding="3">Login</Heading>
+
+      <FormControl id="formSubmit" className="email" onSubmit={handleFormSubmit}>
+        <FormLabel>Email address</FormLabel>
+        <Input type="email" placeholder="youremail@test.com"
+            name="email"
+            id="email"
+            onChange={handleChange}/>
+        <FormLabel>Password</FormLabel>
+        <Input placeholder="******" type="password"
+            name="password"            
+            id="pwd"
+            onChange={handleChange} />
+          {
+          error ? 
+            <Container>
+              <Text fontSize={{ base: "8px", md: "12px", lg: "16px" }}>The provided credentials are incorrect</Text>
+            </Container> : null
+        }
+        <Button
+            mt={4}
+            colorScheme="teal"
+            isLoading={props.isSubmitting}
+            type="submit"
+          >
+            Submit
+          </Button>
+      </FormControl>
+
+{/*       <form onSubmit={handleFormSubmit}>
         <div className="flex-row space-between my-2">
           <label htmlFor="email">Email address:</label>
           <input
@@ -65,7 +94,7 @@ function Login(props) {
             Submit
           </button>
         </div>
-      </form>
+      </form> */}
     </div>
   );
 }
